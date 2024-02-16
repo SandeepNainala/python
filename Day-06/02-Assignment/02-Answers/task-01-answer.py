9c7455a947e8f7cd3e575b681c3825d5536ba795
sandeepnaidu <nainala_sandeep@yahoo.com>
2024-02-16 16:03:52 +0530

    Python assignment1

---
+++ Day-06/02-Assignment/02-Answers/task-01-answer.py
@@ -0,0 +1,13 @@
+a = 10
+b = 5
+
+sum_result = a + b
+different_result = a - b
+product_result = a * b
+quotient_result = a / b
+
+print("Sum:", sum_result)
+print("Difference:", different_result)
+print("Product:", product_result)
+print("Quotient:", quotient_result)
+
