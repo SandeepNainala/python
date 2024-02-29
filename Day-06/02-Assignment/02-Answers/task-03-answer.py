4d77cac0e7bce26281b7f35af6444ec72f3f8360
sandeepnaidu <nainala_sandeep@yahoo.com>
2024-02-29 17:11:21 +0530

    Python assignment3

---
+++ Day-06/02-Assignment/02-Answers/task-03-answer.py
@@ -0,0 +1,15 @@
+x = True
+y = False
+
+and_result = x and y
+or_result = x or y
+not_result_x = not x
+not_result_y = not y
+
+print("x and y:", and_result)
+print("x or y:", or_result)
+print("not_x:", not_result_x)
+print("not_y:", not_result_y)
+
+
+
